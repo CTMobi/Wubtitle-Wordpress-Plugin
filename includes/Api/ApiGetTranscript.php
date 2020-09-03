3d15e72a871fee6e2fb09db4731db801bbbc5b11
Alessio Catania <alessio.catania@ctmobi.it>
2020-09-03 18:27:08 +0200

    remove unused function

--- includes/Api/ApiGetTranscript.php
+++ includes/Api/ApiGetTranscript.php
@@ -23,7 +23,6 @@ class ApiGetTranscript {
 	 * @return void
 	 */
 	public function run() {
-		add_action( 'wp_ajax_get_transcript', array( $this, 'get_transcript' ) );
 		add_action( 'wp_ajax_get_transcript_embed', array( $this, 'get_transcript_embed' ) );
 		add_action( 'wp_ajax_get_transcript_internal_video', array( $this, 'get_transcript_internal_video' ) );
 		add_action( 'wp_ajax_get_video_info', array( $this, 'get_video_info' ) );
@@ -166,68 +165,6 @@ class ApiGetTranscript {
 			wp_send_json_success( $response );
 		}
 		wp_send_json_success( $posts[0]->ID );
-	}
-
-	/**
-	 * Get transcript.
-	 *
-	 * @return void
-	 */
-	public function get_transcript() {
-		if ( ! isset( $_POST['url'], $_POST['source'], $_POST['from'], $_POST['_ajax_nonce'] ) ) {
-			wp_send_json_error( __( 'An error occurred while creating the transcriptions. Please try again in a few minutes', 'wubtitle' ) );
-		}
-		$nonce = sanitize_text_field( wp_unslash( $_POST['_ajax_nonce'] ) );
-		check_ajax_referer( 'itr_ajax_nonce', $nonce );
-		$url_video = sanitize_text_field( wp_unslash( $_POST['url'] ) );
-		$source    = sanitize_text_field( wp_unslash( $_POST['source'] ) );
-		$from      = sanitize_text_field( wp_unslash( $_POST['from'] ) );
-
-		switch ( $source ) {
-			case 'youtube':
-				$video_source = new YouTube();
-				break;
-			default:
-				return;
-		}
-
-		$url_parts    = wp_parse_url( $url_video );
-		$allowed_urls = array(
-			'www.youtube.com',
-			'www.youtu.be',
-		);
-		if ( ! in_array( $url_parts['host'], $allowed_urls, true ) ) {
-			wp_send_json_error( __( 'Url not a valid youtube url', 'wubtitle' ) );
-		}
-		$query_params = array();
-		parse_str( $url_parts['query'], $query_params );
-		$id_video = $query_params['v'];
-
-		$data_posts = $this->get_data_transcript( $id_video, $from );
-		if ( $data_posts ) {
-			wp_send_json_success( $data_posts );
-		}
-
-		$response      = $video_source->send_job_to_backend( $id_video );
-		$response_code = wp_remote_retrieve_response_code( $response );
-		$message       = array(
-			'400' => __( 'An error occurred while creating the transcriptions. Please try again in a few minutes', 'wubtitle' ),
-			'401' => __( 'An error occurred while creating the transcriptions. Please try again in a few minutes', 'wubtitle' ),
-			'403' => __( 'Unable to create transcriptions. Invalid product license', 'wubtitle' ),
-			'500' => __( 'Could not contact the server', 'wubtitle' ),
-			'429' => __( 'Error, no more video left for your subscription plan', 'wubtitle' ),
-		);
-		if ( 201 !== $response_code ) {
-			wp_send_json_error( $message[ $response_code ] );
-		}
-
-		$transcript = $video_source->get_subtitle( $id_video, $from );
-
-		if ( ! $transcript ) {
-			wp_send_json_error( __( 'Transcript not avaiable for this video.', 'wubtitle' ) );
-		}
-
-		wp_send_json_success( $transcript );
 	}
 	/**
 	 * Gets data if post exists and returns it.
