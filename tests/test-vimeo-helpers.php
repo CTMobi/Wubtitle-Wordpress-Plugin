8c74d3a8ed6d99b1c49de3578f5c28ddb2dd3606
Alessio Catania <alessio.catania@ctmobi.it>
2020-09-09 12:36:41 +0200

    test vimeo helpers

---
+++ tests/test-vimeo-helpers.php
@@ -0,0 +1,33 @@
+<?php
+use Wubtitle\Utils\VimeoHelper;
+/**
+ * Class VimeoHelper
+ *
+ * @package Wubtitle
+ */
+
+/**
+ * Test VimeoHelper.
+ */
+class TestVimeoHelper extends WP_UnitTestCase {
+    /**
+     * Setup function.
+     */
+    public function SetUp(){
+        parent::setUp();
+        $this->instance = new VimeoHelper;
+    }
+
+    /**
+     * Test get languages. 
+     */
+    public function test_get_languages() {
+
+        $response = $this->instance->get_languages( );
+
+        $expected_response = 'Thai';
+        $this->assertEquals( $expected_response, $response['th'] );
+    }
+
+
+}
