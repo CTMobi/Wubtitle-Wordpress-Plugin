d82b518e0060fdd1dbe36373d8683ac82a1ad11e
Damiano Di Pietro <damiano.dipietro@ctmobi.it>
2024-12-05 10:26:11 +0100

    feat(apistorerephrase): handle rephrase post update if it already exists

--- includes/Api/ApiStoreRephrase.php
+++ includes/Api/ApiStoreRephrase.php
@@ -85,11 +85,11 @@ class ApiStoreRephrase {
 
 		// Internal Video.
 		if ( isset( $params['attachmentId'] ) && ! empty( $params['attachmentId'] ) && ! empty( get_post( $params['attachmentId'] ) ) ) {
-			return $this->add_post_rephrase( $rephrase, $params['attachmentId'] );
+			return $this->save_post_rephrase( $rephrase, $params['attachmentId'] );
 		}
 		// External Video.
 		if ( isset( $params['videoId'] ) && ! empty( $params['videoId'] ) ) {
-			return $this->add_post_rephrase( $rephrase, $params['videoId'] );
+			return $this->save_post_rephrase( $rephrase, $params['videoId'] );
 		}
 
 		$error = array(
@@ -108,13 +108,13 @@ class ApiStoreRephrase {
 	}
 
 	/**
-	 * Create post rephrase.
+	 * Save rephrase post.
 	 *
 	 * @param string $rephrase rephrase text.
 	 * @param string $id_video video_id or id_attachment.
 	 * @return WP_REST_Response
 	 */
-	public function add_post_rephrase( $rephrase, $id_video ) {
+	public function save_post_rephrase( $rephrase, $id_video ) {
 		$rephrase_helper = new Rephrase();
 		$rephrase_info   = $rephrase_helper->get_rephrase_info( $id_video );
 
@@ -134,17 +134,21 @@ class ApiStoreRephrase {
 			return $response;
 		}
 
-		$trascript_post_elements = array(
-			'post_title'   => $rephrase_info->title,
-			'post_content' => $rephrase,
-			'post_status'  => 'publish',
-			'post_type'    => 'rephrase',
-			'meta_input'   => array(
-				'wubtitle_rephrase' => $id_video,
-			),
+		$args = array(
+			'post_type'              => 'rephrase',
+			'posts_per_page'         => 1,
+			'meta_key'               => 'wubtitle_rephrase',
+			'meta_value'             => $id_video,
+			'no_found_rows'          => true,
+			'update_post_term_cache' => false,
+			'fields'                 => 'ids',
 		);
+		// @phpstan-ignore-next-line
+		$posts_exist = get_posts( $args );
 
-		$trascript_post_id = wp_insert_post( $trascript_post_elements );
+		$trascript_post_id = empty( $posts_exist ) ?
+			$this->create_rephrase_post( $rephrase_info->title, $rephrase, $id_video ) :
+			$this->update_rephrase_post( $posts_exist[0], $rephrase );
 
 		if ( is_wp_error( $trascript_post_id ) ) {
 			$error = array(
@@ -177,5 +181,45 @@ class ApiStoreRephrase {
 		$response->set_status( 200 );
 
 		return $response;
+	}
+
+	/**
+	 * Create new rephrase post.
+	 *
+	 * @param string $title video title.
+	 * @param string $rephrase rephrase text.
+	 * @param string $id_video video_id or id_attachment.
+	 * @return int|\WP_Error
+	 */
+	public function create_rephrase_post( $title, $rephrase, $id_video ) {
+
+		$trascript_post_elements = array(
+			'post_title'   => $title,
+			'post_content' => $rephrase,
+			'post_status'  => 'publish',
+			'post_type'    => 'rephrase',
+			'meta_input'   => array(
+				'wubtitle_rephrase' => $id_video,
+			),
+		);
+
+		return wp_insert_post( $trascript_post_elements );
+	}
+
+	/**
+	 * Update rephrase post.
+	 *
+	 * @param int    $post_id post ID.
+	 * @param string $rephrase rephrase text.
+	 * @return int|\WP_Error
+	 */
+	public function update_rephrase_post( $post_id, $rephrase ) {
+
+		$postarr = array(
+			'ID'           => $post_id,
+			'post_content' => $rephrase,
+		);
+
+		return wp_update_post( $postarr );
 	}
 }
